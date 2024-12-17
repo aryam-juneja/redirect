a2119c97bdbdf4b04417b663a3913074af0b1707
eldoCipher52 <140212831+aryam-juneja@users.noreply.github.com>
2024-12-17 18:10:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Redirect to the desired URL
-    return redirect('https://www.example.com', code=302)
+    return redirect('https://www.google.com', code=302)
 
 if __name__ == '__main__':
     app.run(debug=True)
