af6d5b52731f936bc03cf2f3f2910ffed9756d63
eldoCipher52 <140212831+aryam-juneja@users.noreply.github.com>
2024-12-17 18:05:52 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+# Redirect route
+@app.route('/')
+def home():
+    # Redirect to the desired URL
+    return redirect('https://www.example.com', code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
